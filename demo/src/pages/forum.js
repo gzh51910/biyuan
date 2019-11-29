796a88c4f0f0ea6fd8fa75bdd50f8c19df6e3d68
chenshaobin <635805202@qq.com>
2019-11-29 10:47:26 +0800

    首页UI-优化引入mobile.ant框架造成的margin问题

--- demo/src/pages/forum.js
+++ demo/src/pages/forum.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { biyuan } from '../api'
 import { connect } from 'react-redux';
-
+import '../css/forum.css';
 function mapStateToProps(state) {
     return {
 
@@ -11,8 +11,13 @@ function mapStateToProps(state) {
 class forum extends Component {
     render() {
         return (
-            <div>
-                论坛
+            <div className="container-forum">
+                <header>
+                    <div className="header-home"><img src="http://m.coingogo.com/wap/image/logo.png"/></div>
+                    <div className="header-center">泡论坛</div>
+                    <Icon type="search" />
+                    <Icon type="plus" />
+                </header>
             </div>
         );
     }
