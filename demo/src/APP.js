632a19bfb67efca197da1f67c9ec296cb69d08b6
ying31 <anna1010829421@163.com>
2019-12-05 17:48:39 +0800

    解决冲突

--- demo/src/APP.js
+++ demo/src/APP.js
@@ -9,13 +9,10 @@ import News from "~/News";
 import data from "~/data";
 import forum from "~/forum/forum";
 import mine from "~/mine";
-<<<<<<< HEAD
 import Login from '~/Login';
 import loginargument from '~/loginargument';
-=======
 import forumArticle from "~/forum/forumArticle";
 
->>>>>>> 9da75025a2c67e309894fe58356e13d886ad6143
 import { Menu, Icon } from "antd";
 
 class App extends Component {
@@ -73,7 +70,6 @@ class App extends Component {
   render() {
     return (
       <div className="container-app">
-<<<<<<< HEAD
 
         <Switch>
           <Route path="/home" component={Home} />
@@ -83,26 +79,16 @@ class App extends Component {
           <Route path="/mine" component={mine} />
           <Route path="/Login" component={Login} />
           <Route path="/loginargument" component={loginargument} />
+          <Route path="/foruminfo/:id:fname" component={forumArticle} />
           <Route path="/notfound" render={() => <div>404页面</div>} />
+
           <Redirect from="/" to="/home" exact />
           <Redirect to="/notfound" />
         </Switch>
         <div className="footer">
-=======
-        
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/news" component={News} />
-            <Route path="/data" component={data} />
-            <Route path="/forum" component={forum} />
-            <Route path="/mine" component={mine} />
-            <Route path="/foruminfo/:id:fname" component={forumArticle}/>
-            <Route path="/notfound" render={() => <div>404页面</div>} />
-            <Redirect from="/" to="/home" exact />
-            <Redirect to="/notfound" />
-          </Switch>
-          <div className="footer">
->>>>>>> 9da75025a2c67e309894fe58356e13d886ad6143
+
+
+
           <Menu
             onClick={this.goto}
             selectedKeys={[this.state.currentPath]}
