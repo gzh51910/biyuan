ccef826fcdb5c8c426ef1785ac15a60be3d69e67
dabaoshu <user.name>
2019-12-07 15:41:16 +0800

    app上线1.0

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './store'
 import App from './APP'
 render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       {/* <Route component={App}/> */}
         <App/>
-    </HashRouter>
+    </BrowserRouter>
     </Provider>,
     document.querySelector('#app')
 )
